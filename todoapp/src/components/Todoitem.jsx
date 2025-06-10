f81aea617c91e3356ab6257f8baa913e7d300473
Monday6984 <ofem302@gmail.com>
2025-06-10 18:10:15 +0100

    my-todo-app

---
+++ todoapp/src/components/Todoitem.jsx
@@ -0,0 +1,39 @@
+import styles from "./todoitem.module.css";
+
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log("Delete button clicked", item);
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+
+  function handleClick(name) {
+    //console.log("Item text clicked", name);
+
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+    //console.log(todos);
+  }
+  const checkClass = item.done ? styles.completed : "";
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        <span className={checkClass} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            X
+          </button>
+        </span>
+      </div>
+      <hr className={styles.line} />
+    </div>
+  );
+}
